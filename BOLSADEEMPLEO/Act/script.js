8602b622212cd12fce1ca8a5925091a0dfc15845
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-07-13 00:27:45 -0500

    Add files via upload

--- BOLSADEEMPLEO/Act/script.js
+++ BOLSADEEMPLEO/Act/script.js
@@ -1,25 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Obtener el valor almacenado del correo electrónico
-  const email = localStorage.getItem('email');
-  
-  // Rellenar el campo de correo en el formulario si se encuentra almacenado
+  // Obtener el valor del parámetro 'email' de la URL
+  const urlParams = new URLSearchParams(window.location.search);
+  const email = urlParams.get('email');
+  const prefillValue = urlParams.get('prefill');
+
+  // Rellenar el campo de correo en el formulario si se proporcionó un correo electrónico
   if (email) {
-    document.getElementById('email').value = email;
+    document.getElementById('email').value = decodeURIComponent(email);
   }
-  
+
+  // Rellenar el campo con la palabra preestablecida si se proporcionó
+  if (prefillValue) {
+    document.getElementById('campoPreestablecido').value = decodeURIComponent(prefillValue);
+  }
+
   // Escuchar el evento submit del formulario
   document.getElementById('registrationForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Evitar el envío del formulario
-  
+
     // Obtener el valor del campo de correo electrónico
     const email = document.getElementById('email').value;
-  
+
     // Realizar las acciones necesarias con el correo electrónico, por ejemplo, enviarlo a un servidor, almacenarlo en una base de datos, etc.
     console.log('Correo electrónico registrado:', email);
-    
-    // Almacenar temporalmente el correo electrónico en el almacenamiento local del navegador
-    localStorage.setItem('email', email);
-    
+
     // Redirigir al usuario a otra página después de registrar el correo electrónico
     window.location.href = 'gracias.html';
   });
